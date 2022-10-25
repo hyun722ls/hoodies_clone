d03b3d2809719a2806ab53fd45e9250e2c0bc2d8
okorion <ok.or.orion@gmail.com>
2022-10-26 02:59:10 +0900

    Feat: Modify main.js

--- frontend/hoodies/src/feature/main/main.js
+++ frontend/hoodies/src/feature/main/main.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import { freePreview, previewPros } from "../../common/data/dummyData";
+// import { freePreview, previewPros } from "../../common/data/dummyData";
+import { previewPros } from "../../common/data/dummyData";
 import Header from "../../common/UI/header/header";
 import Articles from "./mainComponent/articles";
 import PopularText from "./mainComponent/popularText";
 import Staffs from "./mainComponent/staffs";
+import axios from 'axios';
 
 const Main = () => {
   const [articles, setArticles] = useState([]);
@@ -11,14 +13,23 @@ const Main = () => {
   const [staffs, setStaffs] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    const response = axios.get('/preview/free');
+    const response1 = axios.get('/preview/popular');
+    // const response2 = axios.get(??) api 만들어지면 스웨거에서 주소 확인
+    
     // const response = 자유게시글 최근 5개 요청
     // const response1 = 추천 많은 글 5개 요청
     // const response2 = 많은 평가 받은 컨설턴트, 프로 정보(향후는 최근)
     // setArticles(response)
     // setPopularText(response1)
     // setStaffs(response2)
-    setArticles(freePreview);
-    setPopularText(freePreview);
+
+    setArticles(response);
+    setPopularText(response1);
+    // setStaffs(response2); api 만들어지면 넣기 line33 끄기
+    
+    // setArticles(freePreview);
+    // setPopularText(freePreview);
     setStaffs(previewPros);
     setIsLoading(false);
   }, []);
