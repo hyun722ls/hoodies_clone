25cc2186814984f3c44ced20b5e828166f197deb
kms940125 <kms940125@hanmail.net>
2022-10-31 19:14:10 +0900

    TEST: modify backend url

--- Hoodies/src/main/java/com/ssafy/hoodies/controller/MainController.java
+++ Hoodies/src/main/java/com/ssafy/hoodies/controller/MainController.java
@@ -1,15 +1,15 @@
-package com.ssafy.hoodies.controller;
+// package com.ssafy.hoodies.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class MainController {
+// @RestController
+// public class MainController {
 
-    @GetMapping("/")
-    public String index() throws Exception {
-        return "index";
-    }
+//     @GetMapping("/")
+//     public String index() throws Exception {
+//         return "index";
+//     }
 
-}
+// }
