38b1a07ffc9c91b8d2a516041181045b6a7f5bb8
조준식 <ororwnstlr@naver.com>
2022-11-03 14:08:38 +0900

    TEST: file upload

---
+++ Hoodies/src/main/java/com/ssafy/hoodies/controller/asfs.java
@@ -0,0 +1,48 @@
+package com.ssafy.hoodies.controller;
+
+import com.ssafy.hoodies.config.security.JwtTokenProvider;
+import com.ssafy.hoodies.model.repository.TokenRepository;
+import com.ssafy.hoodies.model.repository.UserAuthRepository;
+import com.ssafy.hoodies.model.repository.UserRepository;
+import com.ssafy.hoodies.model.service.FileService;
+import com.ssafy.hoodies.model.service.UserService;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Api(tags = {"파일 업로드 API"})
+@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/gallery")
+public class GalleryController {
+    private static final String SUCCESS = "200";
+    private static final String FAIL = "403";
+    private static final String EXPIRED = "400";
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserService userService;
+    private final UserRepository userRepository;
+    private final UserAuthRepository userAuthRepository;
+    private final TokenRepository tokenRepository;
+    private final FileService s3Service;
+
+
+    @PostMapping
+    public Map<String, Object> fileUpload(MultipartFile file) throws IOException {
+        Map<String, Object> resultMap = new HashMap<>();
+
+        try {
+            s3Service.upload(file);
+            resultMap.put("statusCode", SUCCESS);
+        } catch (Exception e) {
+            resultMap.put("statusCode", FAIL);
+        }
+        return resultMap;
+    }
+
+}
