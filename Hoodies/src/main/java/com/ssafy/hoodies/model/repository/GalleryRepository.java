d4d1b07b04497e26029d234dfb96b01d891647f1
조준식 <ororwnstlr@naver.com>
2022-11-03 11:30:20 +0900

    FEAT: file upload and random name setting

--- Hoodies/src/main/java/com/ssafy/hoodies/model/repository/GalleryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.hoodies.model.repository;
-
-import com.ssafy.hoodies.model.entity.Gallery;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GalleryRepository extends JpaRepository<Gallery, Long> {
-}
