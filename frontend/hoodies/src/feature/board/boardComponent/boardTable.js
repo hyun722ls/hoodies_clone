b5b060e8353c9da9858d9276a4da747b0e5d386e
김무종 <kms940125@hanmail.net>
2022-11-04 14:10:10 +0900

    Update boardTable.js

--- frontend/hoodies/src/feature/board/boardComponent/boardTable.js
+++ frontend/hoodies/src/feature/board/boardComponent/boardTable.js
@@ -142,7 +142,18 @@ const BoardTable = (props) => {
   const history = useHistory();
 
   const detailPageHandler = (article) => {
-    history.push({ pathname: "/board/free/detail", state: article._id });
+      if (article.reporter?.length > blockCnt){
+      alert('신고 누적된 게시글입니다.')
+    } else {
+      if (article.type === 1){
+        history.push({ pathname: "/board/free/detail", state: article._id });
+    
+      } else {
+        history.push({ pathname: "/board/annoymous/detail", state: article._id });
+      }
+    
+
+    }
   };
 
   const isFilter = (article) => {
