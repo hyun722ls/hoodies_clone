061d108caefcbf0f9e75d4d04c430f047d0c5a7c
SSAFY <hyun722ls@gmail.com>
2022-11-07 13:31:31 +0900

    FEAT: add feedback

--- frontend/hoodies/src/feature/auth/admin/adminPage.js
+++ frontend/hoodies/src/feature/auth/admin/adminPage.js
@@ -1,11 +1,19 @@
 import { useEffect } from "react"
 import { useState } from "react"
+import Header from "../../../common/UI/header/header"
 
 const AdminPage =() => {
     const [inquiries, setInquiries] = useState([])
 
 
-    return <div></div>
+    return (
+    <div>
+        <Header />
+        <Container>
+        <BoardTable articles={articles} />
+        </Container>
+    </div>
+    )
 }
 
 
