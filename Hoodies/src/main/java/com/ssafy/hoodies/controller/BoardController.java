c5ca58224060e71e735dfdb1d6d50ab5a2ab488a
임재훈 <arza@naver.com>
2022-11-11 16:27:27 +0900

    Update BoardController.java

--- Hoodies/src/main/java/com/ssafy/hoodies/controller/BoardController.java
+++ Hoodies/src/main/java/com/ssafy/hoodies/controller/BoardController.java
@@ -20,7 +20,7 @@ import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.util.*;
 
 @RequiredArgsConstructor
 @RestController
