39817f7fc5c53b8ab53505d9fb52dd08eeac3bcd
zzunsik <ororwnstlr@naver.com>
2022-11-18 11:52:45 +0900

    REFACTOR: file upload, signup

--- Hoodies/src/main/java/com/ssafy/hoodies/controller/FileController.java
+++ Hoodies/src/main/java/com/ssafy/hoodies/controller/FileController.java
@@ -1,25 +1,16 @@
 package com.ssafy.hoodies.controller;
 
-import com.mongodb.client.result.UpdateResult;
-import com.ssafy.hoodies.model.entity.Board;
-import com.ssafy.hoodies.model.repository.BoardRepository;
 import com.ssafy.hoodies.model.service.FileService;
-import com.ssafy.hoodies.model.service.SecurityService;
-import com.ssafy.hoodies.model.service.UserService;
-import com.ssafy.hoodies.util.util;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Api(tags = {"파일 API"})
@@ -27,15 +18,8 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 public class FileController {
-    private final MongoTemplate mongoTemplate;
-    private final BoardRepository boardRepository;
 
-    private final SecurityService securityService;
-    private final UserService userService;
     private final FileService fileService;
-
-    @Value("${nickname.salt}")
-    private String salt;
 
     @PostMapping("/file/{id}")
     @ApiOperation(value = "파일 업로드")
@@ -43,56 +27,15 @@ public class FileController {
         JSONObject json = new JSONObject();
         int statusCode = 400;
 
-        String email = securityService.findEmail();
-        String nickname = userService.findNickname(email);
-        String encodedNickname = util.getEncryptStr(nickname, salt);
-
-        try {
-            Board board = boardRepository.findById(id).get();
-            String writer = board.getWriter();
-
-            // 권한이 없는 요청
-            if (!(writer.equals(nickname) || writer.equals(encodedNickname))) {
-                json.put("statusCode", statusCode);
-                return json;
-            }
-
-            // 기존 업로드 파일 삭제
-            List<String> getFilePaths = board.getFilePaths();
-            if (getFilePaths != null) {
-                for (String path : getFilePaths) {
-                    fileService.deleteFile(path);
-                }
-            }
-
-            // 파일 업로드
-            List<String> filePaths = new ArrayList<>();
-            if (files != null) {
-                for (int i = 0; i < Math.min(files.size(), 4); i++) {
-                    MultipartFile file = files.get(i);
-                    boolean randomFilenameFlag = true;
-                    if (getFilePaths != null && getFilePaths.contains(file.getOriginalFilename()))
-                        randomFilenameFlag = false;
-                    String path = fileService.upload(file, randomFilenameFlag);
-                    // upload에 실패한 경우
-                    if (path.equals("fail"))
-                        continue;
-                    filePaths.add(path);
-                }
-            }
-
-            Query boardQuery = new Query(Criteria.where("_id").is(id));
-            Update boardUpdate = new Update();
-            boardUpdate.set("filePaths", filePaths);
-            UpdateResult ur = mongoTemplate.updateFirst(boardQuery, boardUpdate, "board");
-
-            statusCode = 200;
-            json.put("filePaths", filePaths);
+        List<String> filePaths = fileService.uploadFile(id, files);
+        if (filePaths == null) {
             json.put("statusCode", statusCode);
-        } catch (Exception e) {
-            statusCode = 400;
-            json.put("statusCode", statusCode);
+            return json;
         }
+
+        statusCode = 200;
+        json.put("filePaths", filePaths);
+        json.put("statusCode", statusCode);
         return json;
     }
 }
--- Hoodies/src/main/java/com/ssafy/hoodies/model/service/FileService.java
+++ Hoodies/src/main/java/com/ssafy/hoodies/model/service/FileService.java
@@ -1,84 +1,11 @@
 package com.ssafy.hoodies.model.service;
 
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.j256.simplemagic.ContentInfo;
-import com.j256.simplemagic.ContentInfoUtil;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.PostConstruct;
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.UUID;
+import java.util.List;
 
-@Service
-@NoArgsConstructor
-public class FileService {
-    private AmazonS3 amazonS3;
+public interface FileService {
 
-    @Value("${cloud.aws.credentials.accessKey}")
-    private String accessKey;
+    public List<String> uploadFile(String id, List<MultipartFile> files);
 
-    @Value("${cloud.aws.credentials.secretKey}")
-    private String secretKey;
-
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
-
-    @Value("${cloud.aws.region.static}")
-    private String region;
-
-    private final HashSet<String> availableExtensions = new HashSet<>(Arrays.asList("jpeg", "png", "gif"));
-
-    @PostConstruct
-    public void setAmazonS3() {
-        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
-
-        amazonS3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(this.region).build();
-    }
-
-    public String upload(MultipartFile file, boolean randomFilenameFlag) {
-        String uploadResult = "fail";
-        try (InputStream getFileInputStream = file.getInputStream()) {
-            StringBuilder filename = new StringBuilder();
-            if (randomFilenameFlag)
-                filename.append(UUID.randomUUID()).append("_");
-            filename.append(file.getOriginalFilename());
-
-            ContentInfoUtil contentInfoUtil = new ContentInfoUtil();
-            ContentInfo fileInfo = contentInfoUtil.findMatch(getFileInputStream);
-            String fileExtension = fileInfo.getName();
-
-            // 업로드할 수 없는 확장자일 경우
-            if (!availableExtensions.contains(fileExtension)) {
-                return uploadResult;
-            }
-
-            ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentType(file.getContentType());
-            objectMetadata.setContentLength(file.getSize());
-            PutObjectRequest objectRequest = new PutObjectRequest(bucket, filename.toString(), file.getInputStream(), objectMetadata)
-                    .withCannedAcl(CannedAccessControlList.PublicRead);
-            amazonS3.putObject(objectRequest);
-            uploadResult = filename.toString();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return uploadResult;
-    }
-
-    public void deleteFile(String fileName) {
-        amazonS3.deleteObject(new DeleteObjectRequest(bucket, fileName));
-    }
 }
